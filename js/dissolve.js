462c6d975b57547fcd1668d0e58166ab88cd2d7a
WickerPrison <cobylax17@gmail.com>
2024-04-20 16:20:30 -0600

    ready for testing

--- js/dissolve.js
+++ js/dissolve.js
@@ -1,3 +1,35 @@
+const vertShader = `attribute vec2 aVertexPosition;
+attribute vec2 aTextureCoord;
+
+uniform mat3 projectionMatrix;
+
+varying vec2 pos;
+
+void main(void){
+    gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+    pos = aTextureCoord;
+}`;
+
+const fragShader = `varying vec2 pos;
+uniform sampler2D texture;
+uniform float delta;
+
+vec4 transparent = vec4(0.0);
+vec4 mainColor = vec4(0.0823529411764706, 0.231372549019608, 0.313725490196078, 1.0);
+
+void main(void){
+    vec4 perlin = texture2D(texture, pos);
+    vec4 mask = step(perlin, vec4(delta));
+
+    vec4 outColor = mix(transparent, mainColor, vec4(mask.x));
+
+    //vec4 outColor = mix(transparent, vec4(1.0), vec4(mask.x));
+    //vec4 mask2 = step(perlin, vec4(delta - 0.05));
+    //outColor = mix(outColor, mainColor, mask2);
+
+    gl_FragColor = outColor;
+}`
+
 const app = new PIXI.Application({
     backgroundColor: 0x153B50,
     backgroundAlpha: 0
@@ -15,7 +47,7 @@ let vShader = vertShader;
 let fShader = fragShader;
 let uniforms = {
     delta: 0,
-    texture: PIXI.Texture.from("./Images/PerlinNoise.png")
+    texture: PIXI.Texture.from("https://imgs.search.brave.com/RUDoz4srPjriKOWd0E7nUwDgGKVhTVU5TSRAJAbvyf8/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnN0/YWNrLmltZ3VyLmNv/bS9JRjd0MC5wbmc")
 };
 
 const myFilter = new PIXI.Filter(vShader, fShader, uniforms);
@@ -32,18 +64,21 @@ let fadeIn = true;
 let fadeOut = false;
 let nextLocation = "";
 let delta = 1;
+let fadeSpeed = 1.5;
 function animate(){
     if(fadeIn){
-        delta -= 0.1;
+        console.log(app.ticker.FPS);
+        delta -= fadeSpeed * app.ticker.elapsedMS / 1000;
         uniforms.delta = delta;
         if(delta <= 0){
             fadeIn = false;
         }
     }
     else if(fadeOut){
-        delta += 0.1;
+        delta += fadeSpeed * app.ticker.elapsedMS / 1000;
         uniforms.delta = delta;
         if(delta >= 1){
+            fadeOut = false;
             window.location = nextLocation;
         }
     }
@@ -67,4 +102,8 @@ const leavePage = (event) =>{
 const links = document.getElementsByClassName("link-button");
 for(let i = 0; i < links.length; i++){
     links[i].addEventListener("click", leavePage);
+}
+
+window.onload = ()=>{
+    document.getElementById("frame").style.backgroundColor = "#00000000";
 }
--- js/dissolveShader.js
+++ js/dissolveShader.js
@@ -8,8 +8,12 @@ vec4 mainColor = vec4(0.0823529411764706, 0.231372549019608, 0.313725490196078, 
 void main(void){
     vec4 perlin = texture2D(texture, pos);
     vec4 mask = step(perlin, vec4(delta));
-    vec4 outColor = mix(transparent, vec4(1.0), vec4(mask.x));
-    vec4 mask2 = step(perlin, vec4(delta - 0.05));
-    outColor = mix(outColor, mainColor, mask2);
+
+    vec4 outColor = mix(transparent, mainColor, vec4(mask.x));
+
+    //vec4 outColor = mix(transparent, vec4(1.0), vec4(mask.x));
+    //vec4 mask2 = step(perlin, vec4(delta - 0.05));
+    //outColor = mix(outColor, mainColor, mask2);
+
     gl_FragColor = outColor;
 }`
